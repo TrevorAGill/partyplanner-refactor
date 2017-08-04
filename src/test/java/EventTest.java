43008ff0899be679a8bb2ddfb503731b2259b8fc
Trevor Gill <trevor.a.gill@gmail.com>
2017-08-04 09:08:59 -0700

    fail test to instantiate a new event object using default constructor

--- src/test/java/EventTest.java
+++ src/test/java/EventTest.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class EventTest {
-
+    @Test
+    public void newEvent_InstantiatesNewEvent() throws Exception {
+        Event newEvent = new Event();
+        assertEquals(false, newEvent instanceof Event);
+    }
 }
